5022b679a5c94387c4619e91ff923aa5e4682b77
Saurabh <jsaurabh1906@gmail.com>
2024-02-07 15:14:01 +0530

    feat: Add Drum Kit project with HTML, CSS, and JavaScript
    This commit adds all the necessary files for a Drum Kit project, including HTML,JavaScript logic, CSS styles and associated assets such as images and sound files. The project is ready to be used as a simple drum kit simulator.

---
+++ Drum Kit/index.js
@@ -0,0 +1,68 @@
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick() {
+//   alert("hello");
+// }
+var drumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < drumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+// var audio = new Audio("sounds/tom-4.mp3");
+// audio.play();
+
+document.addEventListener("keypress", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
